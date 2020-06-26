6b6aa7eeb51c1ba6b6017a9f12edf6f0b0ff80ca
Henri <henri.klaus.hillemeier@hs-furtwangen.de>
2020-06-26 13:13:05 +0200

    Aufgabe9 test

---
+++ Aufgabe09/script.ts
@@ -0,0 +1,36 @@
+namespace Aufgabe09 {
+    let formData: FormData;
+    let buttonActionHtml: HTMLButtonElement = <HTMLButtonElement> document.getElementById("htmlBut");
+    buttonActionHtml.addEventListener("click", handleClickHtml);
+
+    let submitBut: HTMLButtonElement = <HTMLButtonElement>document.getElementById("jsonBut");
+    submitBut.addEventListener("click", handleClickJson);
+
+    async function handleClickHtml(): Promise<void> {
+        formData = new FormData(document.forms[0]);
+        let url: string = "https://aufgabe08gis2020.herokuapp.com";
+        url += "/html";
+        // tslint:disable-next-line: no-any
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        url += "?" + query.toString();
+
+        let response: Response = await fetch(url);
+        let responseText: string = await response.text();
+        let serverResponse: HTMLElement = <HTMLElement> document.getElementById("serverResponse");
+        serverResponse.innerHTML = responseText;
+    }
+    
+    async function handleClickJson(): Promise<void> {
+
+        formData = new FormData(document.forms[0]);
+        let url: string = "https://aufgabe08gis2020.herokuapp.com";
+        url += "/json";
+        // tslint:disable-next-line: no-any
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        url = url + "?" + query.toString();
+
+        let response: Response = await fetch(url);
+        let responseText: string = await response.json();        
+        console.log(responseText);
+    }
+}
